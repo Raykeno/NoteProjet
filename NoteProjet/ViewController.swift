64d9af120d22986ae056671310391cc305d987fb
Otto Hajdu <raythekeno@gmail.com>
2021-03-11 14:11:10 +0100

    Initial Commit

---
+++ NoteProjet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoteProjet
+//
+//  Created by user191413 on 3/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
