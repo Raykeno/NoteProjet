205c7f413bfa4ffcfd48862f7b8f2aef0018b61c
CyZe <leo.rupil-baladda@edu.univ-fcomte.fr>
2021-03-18 12:17:22 +0100

    note

---
+++ NoteProjet/Note.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+
+class Note {
+    var name: String
+    var contenu: String
+    var date: String
+    
+    init(name: String, contenu: String, date: String) {
+        self.name = name
+        self.contenu = contenu
+        self.date = date
+
+    }
+}
